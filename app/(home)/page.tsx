b004b4e176fa1e783207d768f932cb3803e1f8af
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-11-13 09:36:55 -0300

    feat: add transactions pie chat component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,8 @@ import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
+import { getDashboard } from "../_data/get-dashboard";
+import TransactionsPieChart from "./_components/transactions-pie-chart";
 
 interface HomeProps {
   searchParams: {
@@ -20,6 +22,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   if (monthIsInvalid) {
     redirect("?month=01");
   }
+  const dashboard = await getDashboard(month);
   return (
     <>
       <Navbar />
@@ -28,7 +31,14 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <SummaryCards month={month} />
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
+          <div className="flex flex-col gap-6 overflow-hidden">
+            <SummaryCards month={month} {...dashboard} />
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
+              <TransactionsPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
